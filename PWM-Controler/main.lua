bd5c7aee0e8ba49916f0f4c1a0270163f57030dc
Amatsume Akira <Zyilin98@zyilin98.cc>
2023-09-04 19:50:19 +0800

    修改 PWM判断逻辑/规范ec11引用库

--- PWM-Controler/main.lua
+++ PWM-Controler/main.lua
@@ -22,12 +22,10 @@ local h, t, r = sensor.dht1x(13, true)
 function test1()
     log.info("dht11", h/100,t/100,r)
 end
-gpio.debounce(1, 100)
-gpio.debounce(4, 100)
-
+gpio.debounce(1, 200)
+gpio.debounce(4, 200)
 
 local ec11 = require("ec11")
-
 
 local GPIO_A = 1
 local GPIO_B = 4
@@ -36,19 +34,18 @@ local speed = 0
 local function ec11_callBack(direction)
     if direction == "left" then
         -- 往左选,逆时针
-        speed = speed - 1
+        speed = speed - 5
     else
         -- 往右旋,顺时针
-        speed = speed + 1
+        speed = speed + 5
     end
     if speed > 100 then
-        speed = 5
-
+        speed = 100
     end
     if speed < 0 then
         speed = 0
     end
-    pwm.open(PWM_ID, 200, speed)
+    pwm.open(PWM_ID, 440, speed)
     log.info("pwm", "speed now", speed, "%")
 end
 
