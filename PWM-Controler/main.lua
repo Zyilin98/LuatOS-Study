fabfd2135cacdb1c6d6fff13b99bacb136a120cd
Amatsume Akira <Zyilin98@zyilin98.cc>
2023-09-04 18:21:34 +0800

    test ec11

--- PWM-Controler/main.lua
+++ PWM-Controler/main.lua
@@ -6,63 +6,63 @@ log.info("main", PROJECT, VERSION)
 _G.sys = require("sys")
 
 if wdt then
-    wdt.init(9000)
-    sys.timerLoopStart(wdt.feed, 3000)
+    wdt.init(900)
+    sys.timerLoopStart(wdt.feed, 300)
 end
 
-local PWM_ID = 12
+
+local PWM_ID = 3
 gpio.debounce(6, 100)
 gpio.debounce(7, 100)
 
-pwm.open(PWM_ID, 20000, 1)
+pwm.open(PWM_ID, 200, 1)
 
 local h, t, r = sensor.dht1x(13, true)
 
 function test1()
     log.info("dht11", h/100,t/100,r)
 end
+gpio.debounce(1, 100)
+gpio.debounce(4, 100)
 
-    local speed = 0
+-- 用法实例, 当前支持一定一脉冲
+local ec11 = require("ec11")
 
-    local function increaseSpeed()
-        speed = speed + 5
-        if speed > 50 then
-            speed = 50
-        end
-        log.info("pwm", "speed now", speed, "%")
-        pwm.open(PWM_ID, 1000, speed)
+-- 按实际接线写
+local GPIO_A = 1
+local GPIO_B = 4
+ec11.init(GPIO_A,GPIO_B)
+local speed = 0
+-- 演示接收旋转效果
+local function increaseSpeed()
+    speed = speed + 5
+    if speed > 70 then
+        speed = 70
     end
-    
-    local function decreaseSpeed()
-        speed = speed - 5
-        if speed < 0 then
-            speed = 0
-        end
-        log.info("pwm", "speed now", speed, "%")
-        pwm.open(PWM_ID, 1000, speed)
+    log.info("pwm", "speed now", speed, "%")
+    pwm.open(PWM_ID, 200, speed)
+end
+
+local function decreaseSpeed()
+    speed = speed - 5
+    if speed < 0 then
+        speed = 0
     end
-    
-    sys.taskInit(function()
+    log.info("pwm", "speed now", speed, "%")
+    pwm.open(PWM_ID, 200, speed)
+end
+local function ec11_callBack(direction)
+    if direction == "left" then
+        -- 往左选,逆时针
+        speed = speed - 1
+    else
+        -- 往右旋,顺时针
+        speed = speed + 1
+    end
+    pwm.open(PWM_ID, 200, speed)
+    log.info("pwm", "speed now", speed, "%")
+end
 
-        local increaseButtonPin = 6
-        local decreaseButtonPin = 7
-        local increaseButtonState = 0
-        local decreaseButtonState = 0
-    
-        gpio.setup(increaseButtonPin, function(level)
-            increaseButtonState = level
-            if increaseButtonState == 1 then
-                increaseSpeed()
-            end
-        end, gpio.PULLUP,gpio.BOTH)
-        gpio.setup(decreaseButtonPin, function(level)
-            decreaseButtonState = level
-            if decreaseButtonState == 1 then
-                decreaseSpeed()
-            end
-        end, gpio.PULLUP,gpio.BOTH)
-    end)
-    sys.timerLoopStart(test1, 5000)
-
+sys.subscribe("ec11",ec11_callBack)
 
 sys.run()
